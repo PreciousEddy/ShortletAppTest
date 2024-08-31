6e24a73b62401cb7eb40782be1ca605543fab602
PreciousEddy <edmundprecious23@gmail.com>
2024-08-31 23:45:23 +0100

    update

---
+++ Flask-Api/terraform/kubernetes_resources.tf
@@ -0,0 +1,82 @@
+resource "kubernetes_namespace" "app" {
+  metadata {
+    name = "flask-api"
+  }
+}
+
+resource "kubernetes_deployment" "api_deployment" {
+  metadata {
+    name      = "flask-api"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "flask-api"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "flask-api"
+        }
+      }
+
+      spec {
+        container {
+          name  = "flask-api"
+          image = "gcr.io/${ecstatic-device-434214-c6}/flask-time-api:latest"
+          
+          # Correcting the port block
+          port {
+            container_port = 5000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "api_service" {
+  metadata {
+    name      = "flask-api"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "flask-api"
+    }
+
+    type = "LoadBalancer"
+
+    port {
+      port        = 80
+      target_port = 5000
+    }
+  }
+}
+
+resource "kubernetes_ingress" "api_ingress" {
+  metadata {
+    name      = "flask-api-ingress"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    rule {
+      http {
+        path {
+          backend {
+            service_name = kubernetes_service.api_service.metadata[0].name
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
+}
