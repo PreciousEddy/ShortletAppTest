8a1ae8d21472659ec72c90777fad32c9e29237f6
Precious Edmund <edmundprecious23@gmail.com>
2024-09-03 21:49:16 +0000

    Push

--- Flask-Api/terraform/state.tf
+++ Flask-Api/terraform/state.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = "us-central1"
-}
-
 resource "google_compute_network" "vpc_network" {
   name                    = "vpc-network"
   auto_create_subnetworks = false
