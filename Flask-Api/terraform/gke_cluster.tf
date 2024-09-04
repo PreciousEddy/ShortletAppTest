e5330d22d305b9752df0ee37a4b7c2bdc46c8cb6
Precious Edmund <edmundprecious23@gmail.com>
2024-09-04 07:15:55 +0100

    Update gke_cluster.tf

--- Flask-Api/terraform/gke_cluster.tf
+++ Flask-Api/terraform/gke_cluster.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
   name     = "gke-cluster"
-  location = "us-central1"
+  location = "us-central1-f"
 
   network    = google_compute_network.vpc_network.id
   subnetwork = google_compute_subnetwork.subnet.id
@@ -14,7 +14,7 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_nodes" {
   name       = "node-pool"
   cluster    = google_container_cluster.primary.name
-  location   = "us-central1"
+  location   = "us-central1-f"
   node_count = 1
 
   node_config {
